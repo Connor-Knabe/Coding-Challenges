249ce50bff2489855bcce67028f64057cef5f710
Connor Knabe <connorknabe@gmail.com>
2014-09-27 21:02:32 -0500

    Will work this way but also can be a one liner

--- Warmup-1/moneyTrouble/monkeyTrouble.java
+++ Warmup-1/moneyTrouble/monkeyTrouble.java
@@ -1,16 +1,14 @@
 //We have two monkeys, a and b, and the parameters aSmile and bSmile indicate if each is smiling. We are in trouble if they are both smiling or if neither of them is smiling. Return true if we are in trouble. 
 
-public class monkeyTrouble {
+public boolean monkeyTrouble(boolean aSmile, boolean bSmile) {
 
-	public static void main(String[] args) {
-    	System.out.println("Hello, World");	
 
-    	boolean aSmile = false;
-    	boolean bSmile = false;
-
-    	System.out.println((aSmile && bSmile)||(!aSmile && !bSmile));
-
-	}
-
+    if ((aSmile && bSmile)|| (!aSmile && !bSmile)){
+    
+        return true;
+    } else {
+    
+        return false;
+    }
 
 }
