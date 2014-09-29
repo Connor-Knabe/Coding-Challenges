a4edaef603c6fb4da0bfbff52df0f249b22bf11a
Connor Knabe <connorknabe@gmail.com>
2014-09-29 09:19:11 -0500

    All tests pass

--- Warmup-1/sumDouble/sumDouble.java
+++ Warmup-1/sumDouble/sumDouble.java
@@ -1,9 +1,4 @@
-public class sumDouble {
-    public static void main(String[] args) {
-        int a = 1;
-        int b = 2;
+public int sumDouble(int a, int b) {
+    return(a==b ? 2*(a+b) : a+b);
 
-        return(a==b ? 2*(a+b) : a+b)
-        
-    }
 }
