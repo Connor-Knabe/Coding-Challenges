2d74b1b65ac23b76846abd2cf3eaf6e15693c916
Connor Knabe <connorknabe@gmail.com>
2014-10-03 09:54:29 -0500

    Added documentation

--- Java/String-1/makeAbba/makeAbba.java
+++ Java/String-1/makeAbba/makeAbba.java
@@ -1,8 +1,13 @@
+//Given two strings, a and b, return the result of putting them together in the order abba, e.g. "Hi" and "Bye" returns "HiByeByeHi". 
+//makeAbba("Hi", "Bye") → "HiByeByeHi"
+//makeAbba("Yo", "Alice") → "YoAliceAliceYo"
+//makeAbba("What", "Up") → "WhatUpUpWhat"
+
 public class makeAbba {
     public static void main(String[] args) {
         String a = "Hi";
         String b = "Bye";
-        
+
         
     }
 }
