7e1ae4728f3aa5a6cfb751b4237df42b3ff58fcf
Connor Knabe <ConnorKnabe@gmail.com>
2014-10-04 17:35:44 -0500

    Works

--- Java/String-1/makeOutWord/makeOutWord.java
+++ Java/String-1/makeOutWord/makeOutWord.java
@@ -1,6 +1,12 @@
+/*Given an "out" string length 4, such as "<<>>", and a word, return a new string where the word is in the middle of the out string, e.g. "<<word>>". Note: use str.substring(i, j) to extract the String starting at index i and going up to but not including index j. 
+*/
+
 public class makeOutWord {
     public static void main(String[] args) {
-        
+    	String out1 = "<<>>";
+    	String word = "Yay";
+ ;
+    	System.out.println(out1.substring(0,2)+word+out1.substring(2,4));
         
     }
 }
