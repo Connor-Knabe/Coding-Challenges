eb4a697ba6e36ec13ca7873f2c895da42d68c5d8
Connor Knabe <ConnorKnabe@gmail.com>
2014-10-05 01:33:55 -0500

    Prints out the length of string

--- Java/String-1/extraEnd/extraEnd.java
+++ Java/String-1/extraEnd/extraEnd.java
@@ -1,6 +1,14 @@
+/*Given a string, return a new string made of 3 copies of the last 2 chars of the original string. The string length will be at least 2. 
+
+extraEnd("Hello") → "lololo"
+extraEnd("ab") → "ababab"
+extraEnd("Hi") → "HiHiHi"
+*/
 public class extraEnd {
     public static void main(String[] args) {
-        
+    	String str = "Hello";
+    	System.out.println(str.length()-1);
+    	//System.out.println(str.length()-3,str.length()-1);    
         
     }
 }
