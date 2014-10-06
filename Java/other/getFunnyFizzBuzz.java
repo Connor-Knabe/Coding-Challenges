88f5dd6aadfa72a5692e33e203603534ba6c5c2b
Connor Knabe <ConnorKnabe@gmail.com>
2014-10-06 14:53:06 -0500

    Started on getFunnyFizzBuzz

---
+++ Java/other/getFunnyFizzBuzz.java
@@ -0,0 +1,15 @@
+//Link to problem http://codingbat.com/prob/p223369
+/*
+Write a method that accepts positive integers. If the boolean flag is true then: For multiples of three, the string "Fizz" is returned and for the multiples of five the string "Buzz" is returned. For numbers which are multiples of both three and five print "FizzBuzz". For all other numbers, it returns the string "Cool". If the boolean flag is false, then the string "FizzyBuzzy" is returned all the time. 
+
+getFunnyFizzBuzz(42, false) → "FizzyBuzzy"
+getFunnyFizzBuzz(60, false) → "FizzyBuzzy"
+getFunnyFizzBuzz(3, false) → "FizzyBuzzy"
+*/
+
+public class getFunnyFizzBuzz {
+    public static void main(String[] args) {
+        
+        
+    }
+}
