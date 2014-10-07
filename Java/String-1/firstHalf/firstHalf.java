4afcaa4d29c1d97d20bed49889e744b73a0ec7ca
Connor Knabe <ConnorKnabe@gmail.com>
2014-10-07 13:07:46 -0500

    Added description

--- Java/String-1/firstHalf/firstHalf.java
+++ Java/String-1/firstHalf/firstHalf.java
@@ -1,3 +1,12 @@
+/*
+Given a string of even length, return the first half. So the string "WooHoo" yields "Woo". 
+
+firstHalf("WooHoo") → "Woo"
+firstHalf("HelloThere") → "Hello"
+firstHalf("abcdef") → "abc"
+
+*/
+
 public class firstHalf {
     public static void main(String[] args) {
         
