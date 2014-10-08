c97e4d78658fc2a897950c20e7b5fa5e5db751d4
Connor Knabe <ConnorKnabe@gmail.com>
2014-10-08 15:47:24 -0500

    Added description

--- Java/String-1/nonStart/nonStart.java
+++ Java/String-1/nonStart/nonStart.java
@@ -1,6 +1,15 @@
+/*
+Given 2 strings, return their concatenation, except omit the first char of each. The strings will be at least length 1. 
+
+nonStart("Hello", "There") → "ellohere"
+nonStart("java", "code") → "avaode"
+nonStart("shotl", "java") → "hotlava"
+
+*/
 public class nonStart {
     public static void main(String[] args) {
         
-     System.out.println
+     System.out.println("HI");
+
     }
 }
