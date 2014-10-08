d7cc2922e59e9b708300d47d300024885a16fca3
Connor Knabe <ConnorKnabe@gmail.com>
2014-10-08 15:49:22 -0500

    All tests pass on codingbat first attempt

--- Java/String-1/nonStart/nonStart.java
+++ Java/String-1/nonStart/nonStart.java
@@ -6,10 +6,7 @@ nonStart("java", "code") → "avaode"
 nonStart("shotl", "java") → "hotlava"
 
 */
-public class nonStart {
-    public static void main(String[] args) {
-        
-     System.out.println("HI");
-
-    }
+public String nonStart(String a, String b) {
+  
+    return (a.substring(1)+b.substring(1));  
 }
