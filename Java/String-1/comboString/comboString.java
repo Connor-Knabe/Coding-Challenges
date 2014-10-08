3f9d8112b6edee0911e7b3b8bf7028df96e18312
Connor Knabe <ConnorKnabe@gmail.com>
2014-10-07 21:00:15 -0500

    Prints out properly

--- Java/String-1/comboString/comboString.java
+++ Java/String-1/comboString/comboString.java
@@ -1,6 +1,17 @@
+/*
+
+Given 2 strings, a and b, return a string of the form short+long+short, with the shorter string on the outside and the longer string on the inside. The strings will not be the same length, but they may be empty (length 0). 
+
+comboString("Hello", "hi") → "hiHellohi"
+comboString("hi", "Hello") → "hiHellohi"
+comboString("aaa", "b") → "baaab"
+*/
 public class comboString {
     public static void main(String[] args) {
-        
+        String a = "Hello";
+        String b = "hi";
+
+        System.out.println(b + a + b);
         
     }
 }
