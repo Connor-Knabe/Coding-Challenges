ebef7148385f5bbae6835ce08a9ba3a037305744
Connor Knabe <ConnorKnabe@gmail.com>
2014-10-09 20:07:19 -0500

    All tests pass

--- Java/right2/right2.java
+++ Java/right2/right2.java
@@ -1,8 +1,4 @@
-public class right2 {
-    public static void main(String[] args) {
-        String str = "Hello";
+public String right2(String str) {
+return (str.substring(str.length()-2)+str.substring(0, str.length()-2));
 
-        System.out.println(str.substring(str.length()-2)+str.substring(0, str.length()-2));
-        
-    }
 }
