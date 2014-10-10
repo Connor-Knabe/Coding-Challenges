e5f1026bb829e641983f5d3631d1e149cb695857
Connor Knabe <ConnorKnabe@gmail.com>
2014-10-09 20:05:22 -0500

    Will refactor later

--- Java/right2/right2.java
+++ Java/right2/right2.java
@@ -1,6 +1,10 @@
 public class right2 {
     public static void main(String[] args) {
-        
+        String str = "Hello";
+        String str1 = str.substring(str.length()-2);
+        String str2 = str.substring(0, str.length()-2);
+
+        System.out.println(str.substring(str.length()-2)+str2);
         
     }
 }
