dfca78d4604ee55abea7ee182c90bcb7835c0abf
Connor Knabe <ConnorKnabe@gmail.com>
2014-10-09 20:06:49 -0500

    refactored to one liner

--- Java/right2/right2.java
+++ Java/right2/right2.java
@@ -1,10 +1,8 @@
 public class right2 {
     public static void main(String[] args) {
         String str = "Hello";
-        String str1 = str.substring(str.length()-2);
-        String str2 = str.substring(0, str.length()-2);
 
-        System.out.println(str.substring(str.length()-2)+str2);
+        System.out.println(str.substring(str.length()-2)+str.substring(0, str.length()-2));
         
     }
 }
