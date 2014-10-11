62f909e2c32ab33bc3d85df5aae9e60dc90b6f9d
Connor Knabe <ConnorKnabe@gmail.com>
2014-10-11 14:14:23 -0500

    All tests pass

--- Java/String-1/withouEnd2/withouEnd2.java
+++ Java/String-1/withouEnd2/withouEnd2.java
@@ -7,20 +7,14 @@ withouEnd2("abc") → "b"
 withouEnd2("ab") → ""
 */
 
-public class withouEnd2 {
-    public static void main(String[] args) {
-        String str = "";
-
-        if (str.length()==0){
-        	System.out.println("");
+public String withouEnd2(String str) {
+  
+        if (str.length()<2){
+            return ("");
         } else if (str.length()==2) {
-        	System.out.println("");
-
-    	}
-        else {
-        	System.out.println(str.substring(1,str.length()-1));	
+            return("");
+    	} else {
+            return(str.substring(1,str.length()-1));	
         }
         
-        
-    }
 }
