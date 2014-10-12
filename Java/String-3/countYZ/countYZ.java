20d32f72909b93c57e5f8a9386806c51afbc9eaa
Connor Knabe <ConnorKnabe@gmail.com>
2014-10-12 11:46:03 -0500

    using for loop to loop through string

--- Java/String-3/countYZ/countYZ.java
+++ Java/String-3/countYZ/countYZ.java
@@ -1,6 +1,24 @@
+/*
+
+Given a string, count the number of words ending in 'y' or 'z' -- so the 'y' in "heavy" and the 'z' in "fez" count, but not the 'y' in "yellow" (not case sensitive). We'll say that a y or z is at the end of a word if there is not an alphabetic letter immediately following it. (Note: Character.isLetter(char) tests if a char is an alphabetic letter.) 
+
+countYZ("fez day") → 2
+countYZ("day fez") → 2
+countYZ("day fyyyz") → 2
+
+*/
 public class countYZ {
     public static void main(String[] args) {
-        
+        String str = "fez day";
+        int count = 0
+        ;
+        /*for (int i = 0; i<str.length(); i++){
+        	if ((str.charAt(i)).equals(" ")){
+        		if ((str.charAt(i-1)).equals("y")||(str.charAt(i-1)).equals("z"))
+        			count++;
+        	}*/
+
+        }
         
     }
 }
