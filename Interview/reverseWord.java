0a41f11ea63d2dd27f9db064a18c4278d778caa2
Connor Knabe <ConnorKnabe@gmail.com>
2014-10-13 13:24:52 -0500

    Prints yes if at space in sentence

--- Interview/reverseWord.java
+++ Interview/reverseWord.java
@@ -2,9 +2,12 @@ public class reverseWord {
     public static void main(String[] args) {
         String str = "Hello World";
         String revStr = "olleH dlroW";
-
+        char curLetter;
         for (int i = 0; i<revStr.length(); i++){
-
+        	curLetter = revStr.charAt(i);
+        	if (curLetter == ' '){
+        		System.out.println("YES");
+        	}
         	System.out.println(i);
         }
         
