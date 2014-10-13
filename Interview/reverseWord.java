73cf92eb3e09620429caf5cfd14e69d69c066674
Connor Knabe <ConnorKnabe@gmail.com>
2014-10-13 13:22:02 -0500

    Working on various interview questions

---
+++ Interview/reverseWord.java
@@ -0,0 +1,12 @@
+public class reverseWord {
+    public static void main(String[] args) {
+        String str = "Hello World";
+        String revStr = "olleH dlroW";
+
+        for (int i = 0; i<revStr.length(); i++){
+
+        	System.out.println(i);
+        }
+        
+    }
+}
