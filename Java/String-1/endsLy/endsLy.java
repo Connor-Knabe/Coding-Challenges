7bd27dfa12b88f7fc593f3370f2187e948a69a9e
Connor Knabe <ConnorKnabe@gmail.com>
2014-10-13 13:09:59 -0500

    Added instructions to endsLy

---
+++ Java/String-1/endsLy/endsLy.java
@@ -0,0 +1,15 @@
+/*
+Given a string, return true if it ends in "ly". 
+
+endsLy("oddly") → true
+endsLy("y") → false
+endsLy("oddy") → false
+
+*/
+
+public class endsLy {
+    public static void main(String[] args) {
+        
+        
+    }
+}
