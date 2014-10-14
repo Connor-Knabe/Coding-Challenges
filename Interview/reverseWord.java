3b0356c7a09974dd893de797f14110edf2708aec
Connor Knabe <ConnorKnabe@gmail.com>
2014-10-14 09:15:58 -0500

    Adding print statements for debugging

--- Interview/reverseWord.java
+++ Interview/reverseWord.java
@@ -1,7 +1,7 @@
 public class reverseWord {
 
 	public static void revWord(String revWord){
-		System.out.println(revWord);
+		System.out.println("Rev word coming into method="+ revWord);
         String word1 = "";
        
 		for (int i = revWord.length()-1; i >= 0; i--){
@@ -17,7 +17,7 @@ public class reverseWord {
         char curLetter;
         for (int i = 0; i<revStr.length(); i++){
         	curLetter = revStr.charAt(i);
-        	if (curLetter == ' '){
+        	if (curLetter == ' ' || i==revStr.length()-1){
         		revWord(revStr.substring(0,i));
         		System.out.println("YES");
         	}
