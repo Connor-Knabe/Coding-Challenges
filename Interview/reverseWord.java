44ca2f58527c22b62848de5053d7b2d946d4a421
Connor Knabe <ConnorKnabe@gmail.com>
2014-10-14 09:19:02 -0500

    Reverses words properly now just needs to concatinate them

--- Interview/reverseWord.java
+++ Interview/reverseWord.java
@@ -7,21 +7,23 @@ public class reverseWord {
 		for (int i = revWord.length()-1; i >= 0; i--){
 			word1 += revWord.charAt(i);
 		}
-		System.out.println(word1);
+		System.out.println("Reversed word = "+word1);
 		//return word1;
 	} 
 
     public static void main(String[] args) {
         String str = "Hello World";
         String revStr = "olleH dlroW";
+        int lastWordIndex = 0;
         char curLetter;
         for (int i = 0; i<revStr.length(); i++){
         	curLetter = revStr.charAt(i);
-        	if (curLetter == ' ' || i==revStr.length()-1){
+        	if (curLetter == ' '){
         		revWord(revStr.substring(0,i));
-        		System.out.println("YES");
+        		lastWordIndex = i;
+        	} else if (i==revStr.length()-1){
+        		revWord(revStr.substring(lastWordIndex,i+1));
         	}
-        	System.out.println(i);
         }
         
     }
