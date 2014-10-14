77aa7e4f57adb8cc65e25621e48f403664b97d3c
Connor Knabe <ConnorKnabe@gmail.com>
2014-10-14 09:23:32 -0500

    prints properly but needs to have space

--- Interview/reverseWord.java
+++ Interview/reverseWord.java
@@ -1,6 +1,6 @@
 public class reverseWord {
 
-	public static void revWord(String revWord){
+	public static String revWord(String revWord){
 		System.out.println("Rev word coming into method="+ revWord);
         String word1 = "";
        
@@ -8,23 +8,25 @@ public class reverseWord {
 			word1 += revWord.charAt(i);
 		}
 		System.out.println("Reversed word = "+word1);
-		//return word1;
+		return word1;
 	} 
 
     public static void main(String[] args) {
-        String str = "Hello World";
+        String str = "";
         String revStr = "olleH dlroW";
         int lastWordIndex = 0;
         char curLetter;
         for (int i = 0; i<revStr.length(); i++){
         	curLetter = revStr.charAt(i);
         	if (curLetter == ' '){
-        		revWord(revStr.substring(0,i));
+        		str += revWord(revStr.substring(0,i));
         		lastWordIndex = i;
         	} else if (i==revStr.length()-1){
-        		revWord(revStr.substring(lastWordIndex,i+1));
+        		str += revWord(revStr.substring(lastWordIndex,i+1));
         	}
         }
+
+        System.out.println("Final string is " + str);
         
     }
 }
