365345c34fa6c1ebb4ed69f4ab346233539bb07a
Connor Knabe <ConnorKnabe@gmail.com>
2014-10-16 12:30:04 -0500

    First checks to make sure string can be made from index

--- Java/String-1/twoChar/twoChar.java
+++ Java/String-1/twoChar/twoChar.java
@@ -1,8 +1,19 @@
+/*
+Given a string and an index, return a string length 2 starting at the given index. If the index is too big or too small to define a string length 2, use the first 2 chars. The string length will be at least 2. 
 
+twoChar("java", 0) → "ja"
+twoChar("java", 2) → "va"
+twoChar("java", 3) → "ja"
+*/
 
 public class twoChar {
     public static void main(String[] args) {
-        
+        String str = "java";
+        int index = 4; 
+        if (str.length()>index+1){
+
+        	System.out.println("HII");
+        }
         
     }
 }
