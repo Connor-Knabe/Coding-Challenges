dca3a4104cf0ce8034ec28565dc40bc28c1b2ad8
Connor Knabe <ConnorKnabe@gmail.com>
2014-10-17 10:51:23 -0500

    works but needs to be refactored

--- Java/String-1/middleThree/middleThree.java
+++ Java/String-1/middleThree/middleThree.java
@@ -1,6 +1,8 @@
 public class middleThree {
     public static void main(String[] args) {
-        
+        String str = "Candy";
+        int len = str.length();
+        System.out.println(str.substring((len/2)-1,len/2+2));
         
     }
 }
