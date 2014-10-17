6594362a6b20b6ae4a80162afaf5380de74010a6
Connor Knabe <ConnorKnabe@gmail.com>
2014-10-17 10:53:51 -0500

    Codingbat format all tests pass

--- Java/String-1/middleThree/middleThree.java
+++ Java/String-1/middleThree/middleThree.java
@@ -1,10 +1,7 @@
-public class middleThree {
-    public static void main(String[] args) {
-        String str = "Candy";
-        int len = str.length();
-        int strIndex = (len/2)-1;
+public String middleThree(String str) {
+    int len = str.length();
+    int strIndex = (len/2)-1;
 
-        System.out.println(str.substring(strIndex,strIndex+3));
-        
-    }
+    return(str.substring(strIndex,strIndex+3));
+
 }
