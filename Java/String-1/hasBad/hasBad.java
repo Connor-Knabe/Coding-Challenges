d4605d37e73836b98c8523123c98d3a71dbd6cae
Connor Knabe <ConnorKnabe@gmail.com>
2014-10-18 10:49:23 -0500

    Works for most cases except for 3 letters that don't contain bad

--- Java/String-1/hasBad/hasBad.java
+++ Java/String-1/hasBad/hasBad.java
@@ -13,8 +13,6 @@ public boolean hasBad(String str) {
     if (str.length()<3){
         return false;
     }
-
-    
     if ((str.substring(0,3)).equals("bad") || (str.substring(1,4)).equals("bad") ){
     	return true;
     } else {
