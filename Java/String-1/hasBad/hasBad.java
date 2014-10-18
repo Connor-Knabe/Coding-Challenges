a7558c55db5516e3e85bfd445df3bbb26404fb08
Connor Knabe <ConnorKnabe@gmail.com>
2014-10-18 10:50:16 -0500

    All tests pass

--- Java/String-1/hasBad/hasBad.java
+++ Java/String-1/hasBad/hasBad.java
@@ -13,6 +13,12 @@ public boolean hasBad(String str) {
     if (str.length()<3){
         return false;
     }
+    
+    if (str.length()==3){
+        return (str.substring(0,3).equals("bad"));
+    }
+
+    
     if ((str.substring(0,3)).equals("bad") || (str.substring(1,4)).equals("bad") ){
     	return true;
     } else {
