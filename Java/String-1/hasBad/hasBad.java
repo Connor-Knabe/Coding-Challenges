1f9abb46e345fe34e384e81f11b0c57df17107f4
Connor Knabe <ConnorKnabe@gmail.com>
2014-10-18 10:51:28 -0500

    Codingbat's shorter solution

--- Java/String-1/hasBad/hasBad.java
+++ Java/String-1/hasBad/hasBad.java
@@ -9,19 +9,18 @@ hasBad("xxbadxx") → false
 
 */
 public boolean hasBad(String str) {
+  if (str.length()>=3 && str.substring(0, 3).equals("bad")) {
+    return true;
+  }
+  if (str.length()>=4 && str.substring(1, 4).equals("bad")) {
+    return true;
+  }
+  return false;
   
-    if (str.length()<3){
-        return false;
-    }
-    
-    if (str.length()==3){
-        return (str.substring(0,3).equals("bad"));
-    }
-
-    
-    if ((str.substring(0,3)).equals("bad") || (str.substring(1,4)).equals("bad") ){
-    	return true;
-    } else {
-        return false;
-    }
+  // Solution notes: basically want to call substring(0, 3) and
+  // substring(1, 4) and check if that equals "bad". Need to check
+  // the length first so as to not go off the end of the string.
+  // Alternately one could use indexOf() -- that code is short
+  // but it runs much slower, since it needlessly checks over the whole
+  // string.
 }
