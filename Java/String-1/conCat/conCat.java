9d5b41e8507ab5329d78ebcc18f1fb690cc5df87
Connor Knabe <ConnorKnabe@gmail.com>
2014-10-19 13:10:32 -0500

    Seems to be working

--- Java/String-1/conCat/conCat.java
+++ Java/String-1/conCat/conCat.java
@@ -8,13 +8,15 @@ conCat("abc", "") → "abc"
 
 public class conCat {
     public static void main(String[] args) {
-        String a = "abc";
+        String a = "abca";
         String b = "cat";
 
         if (a.substring(a.length()-1).equals(b.substring(0,1))){
-
+        	System.out.println(a.substring(0,a.length()-1) + b);
 
         	System.out.println("same char");
+        } else {
+        	System.out.println(a+b);
         }
         
     }
