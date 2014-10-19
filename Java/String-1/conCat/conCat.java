66adbaaf17a8728bf632f5801f6aed294d38bebf
Connor Knabe <ConnorKnabe@gmail.com>
2014-10-19 13:12:31 -0500

    Added a check to see if string is empty

--- Java/String-1/conCat/conCat.java
+++ Java/String-1/conCat/conCat.java
@@ -11,10 +11,12 @@ public class conCat {
         String a = "abca";
         String b = "cat";
 
+        if (a.length()<1 || b.length()<1){
+        	System.out.println(a+b);
+
+        }
         if (a.substring(a.length()-1).equals(b.substring(0,1))){
         	System.out.println(a.substring(0,a.length()-1) + b);
-
-        	System.out.println("same char");
         } else {
         	System.out.println(a+b);
         }
