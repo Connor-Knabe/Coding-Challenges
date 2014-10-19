6e8c168647b9d85a60cad5c81551de8ecb0dedf3
Connor Knabe <ConnorKnabe@gmail.com>
2014-10-19 10:36:13 -0500

    All tests pass codingbat format

--- Java/String-1/lastChars/lastChars.java
+++ Java/String-1/lastChars/lastChars.java
@@ -7,18 +7,12 @@ lastChars("yo", "java") → "ya"
 lastChars("hi", "") → "h@"
 */
 
-public class lastChars {
-    public static void main(String[] args) {
-        String a = "last";
-        String b = "chars";
-
-        if (a.length()<1){
-        	a="@";
-        } 
-        if (b.length()<1){
-        	b="@";
-        }
-        System.out.println(a.substring(0,1)+b.substring(b.length()-1));
-        
+public String lastChars(String a, String b) {
+    if (a.length()<1){
+        a="@";
+    } 
+    if (b.length()<1){
+        b="@";
     }
+    return(a.substring(0,1)+b.substring(b.length()-1));
 }
