2bb72561b844ee96841c38f17d1c8fd6e8918ed8
Connor Knabe <ConnorKnabe@gmail.com>
2014-10-19 13:07:35 -0500

    Checks to see if the character is repeated when concatinating

--- Java/String-1/conCat/conCat.java
+++ Java/String-1/conCat/conCat.java
@@ -8,7 +8,14 @@ conCat("abc", "") → "abc"
 
 public class conCat {
     public static void main(String[] args) {
-        
+        String a = "abc";
+        String b = "cat";
+
+        if (a.substring(a.length()-1).equals(b.substring(0,1))){
+
+
+        	System.out.println("same char");
+        }
         
     }
 }
