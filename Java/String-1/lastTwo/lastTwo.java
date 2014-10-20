c86316a7d858582a3b5af9a481734898b6c0640b
Connor Knabe <ConnorKnabe@gmail.com>
2014-10-20 11:48:36 -0500

    Added instructions

--- Java/String-1/lastTwo/lastTwo.java
+++ Java/String-1/lastTwo/lastTwo.java
@@ -1,3 +1,11 @@
+/*
+Given a string of any length, return a new string where the last 2 chars, if present, are swapped, so "coding" yields "codign". 
+
+lastTwo("coding") → "codign"
+lastTwo("cat") → "cta"
+lastTwo("ab") → "ba"
+*/
+
 public class lastTwo {
     public static void main(String[] args) {
         
