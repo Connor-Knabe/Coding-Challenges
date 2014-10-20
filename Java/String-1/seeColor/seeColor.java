a6036d7768d232a0f1c4a5a1940efe0ca55ec884
Connor Knabe <ConnorKnabe@gmail.com>
2014-10-20 11:56:22 -0500

    Started on seeColor problem

---
+++ Java/String-1/seeColor/seeColor.java
@@ -0,0 +1,14 @@
+/*
+Given a string, if the string begins with "red" or "blue" return that color string, otherwise return the empty string. 
+
+seeColor("redxx") → "red"
+seeColor("xxred") → ""
+seeColor("blueTimes") → "blue"
+*/
+
+public class seeColor {
+    public static void main(String[] args) {
+        
+        
+    }
+}
