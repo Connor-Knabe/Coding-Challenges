18f9193362f9bddc08d20b015327e40ea33f2d00
Connor Knabe <ConnorKnabe@gmail.com>
2014-10-21 08:01:58 -0500

    Started on frontAgain problem

---
+++ Java/String-1/frontAgain/frontAgain.java
@@ -0,0 +1,13 @@
+/*
+Given a string, return true if the first 2 chars in the string also appear at the end of the string, such as with "edited". 
+
+frontAgain("edited") → true
+frontAgain("edit") → false
+frontAgain("ed") → true
+*/
+public class frontAgain {
+    public static void main(String[] args) {
+        
+        
+    }
+}
