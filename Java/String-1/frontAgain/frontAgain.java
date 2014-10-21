e60ee2c9bd7c19a200d97b7dba82d8928aea6107
Connor Knabe <ConnorKnabe@gmail.com>
2014-10-21 08:05:06 -0500

    All tests pass

--- Java/String-1/frontAgain/frontAgain.java
+++ Java/String-1/frontAgain/frontAgain.java
@@ -5,13 +5,16 @@ frontAgain("edited") → true
 frontAgain("edit") → false
 frontAgain("ed") → true
 */
-public class frontAgain {
-    public static void main(String[] args) {
-        String str = "edited";
 
-        if (str.substring(0,2).equals(str.substring(str.length()-2))){
-        	System.out.println("True");
-        }
-        
+public boolean frontAgain(String str) {
+
+    if (str.length()<2){
+        return false;
+    }
+    
+    if (str.substring(0,2).equals(str.substring(str.length()-2))){
+        return true;
+    } else {
+        return false;
     }
 }
