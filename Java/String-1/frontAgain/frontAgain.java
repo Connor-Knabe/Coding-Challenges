6047eb3080948d3fb321c192e3e91efa79bc8bca
Connor Knabe <ConnorKnabe@gmail.com>
2014-10-21 08:05:40 -0500

    Refactored

--- Java/String-1/frontAgain/frontAgain.java
+++ Java/String-1/frontAgain/frontAgain.java
@@ -10,11 +10,8 @@ public boolean frontAgain(String str) {
 
     if (str.length()<2){
         return false;
+    } else {
+        return str.substring(0,2).equals(str.substring(str.length()-2));
     }
     
-    if (str.substring(0,2).equals(str.substring(str.length()-2))){
-        return true;
-    } else {
-        return false;
-    }
 }
