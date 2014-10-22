6f29e1375aabbea793b3f5feacae6f766eecb2ee
Connor Knabe <ConnorKnabe@gmail.com>
2014-10-22 09:24:28 -0500

    All tests pass

--- Java/String-1/minCat/minCat.java
+++ Java/String-1/minCat/minCat.java
@@ -6,24 +6,15 @@ minCat("Hello", "java") → "ellojava"
 minCat("java", "Hello") → "javaello"
 */
 
-public class minCat {
-    public static void main(String[] args) {
-    	String a = "x";
-    	String b = "abc";
-    	String finalStr = "";
-    	if (a.length()>b.length()){
-    		finalStr = a.substring(a.length()-b.length());
-    		finalStr += b;
-    		System.out.println("A longer" + finalStr);
-    	} else {
-    		finalStr += a;
-    		finalStr += b.substring(b.length()-a.length());
-
-    		System.out.println("B longer" + finalStr);
-
-    	}
-
-        
-        
+public String minCat(String a, String b) {
+    String finalStr = "";
+    if (a.length()>b.length()){
+        finalStr = a.substring(a.length()-b.length());
+        finalStr += b;
+        return(finalStr);
+    } else {
+        finalStr += a;
+        finalStr += b.substring(b.length()-a.length());
+        return(finalStr);
     }
 }
