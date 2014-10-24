4c6fabeb6bb4b2b752459ae5236070c39e2346d5
Connor Knabe <ConnorKnabe@gmail.com>
2014-10-24 09:33:26 -0500

    All tests pass

--- Java/Logic-1/old35/old35.java
+++ Java/Logic-1/old35/old35.java
@@ -4,12 +4,14 @@ Return true if the given non-negative number is a multiple of 3 or 5, but not bo
 old35(3) → true
 old35(10) → true
 old35(15) → false
-
 */
 
-public class old35 {
-    public static void main(String[] args) {
-        
-        
-    }
+public boolean old35(int n) {
+   if (n % 3 == 0 && n % 5 == 0){
+        	return false;
+        } else if (n % 3 == 0 || n % 5 == 0){
+        	return true;
+        } else {
+        	return false;
+        }
 }
