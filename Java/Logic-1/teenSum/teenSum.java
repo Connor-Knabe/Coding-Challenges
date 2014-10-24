dfc1decfea7bdb2cd79e9102e3d03caa930dbe60
Connor Knabe <ConnorKnabe@gmail.com>
2014-10-24 09:52:17 -0500

    All tests pass

--- Java/Logic-1/teenSum/teenSum.java
+++ Java/Logic-1/teenSum/teenSum.java
@@ -1,6 +1,18 @@
-public class teenSum {
-    public static void main(String[] args) {
-        
-        
+/*
+Given 2 ints, a and b, return their sum. However, "teen" values in the range 13..19 inclusive, are extra lucky. So if either value is a teen, just return 19. 
+
+teenSum(3, 4) → 7
+teenSum(10, 13) → 19
+teenSum(13, 2) → 19
+*/
+public int teenSum(int a, int b) {
+
+    if (a > 12 && a < 20){
+        return 19;
+    } else if (b > 12 && b < 20) {
+        return 19;
+    } else {
+        return a + b;
     }
+  
 }
