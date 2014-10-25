c144b3ef054891538ec7db23a6a029631b3199c4
Connor Knabe <ConnorKnabe@gmail.com>
2014-10-25 11:13:13 -0500

    Verifies to see if the sums are 10 more than a + b

--- Java/Logic-1/blueTicket/blueTicket.java
+++ Java/Logic-1/blueTicket/blueTicket.java
@@ -10,5 +10,7 @@ blueTicket(6, 1, 4) → 10
 public int blueTicket(int a, int b, int c) {
     if (a+b || b+c || a+c){
         return 10;
-    }
+    } else if (a+b == (a+b+10) || a+b == (a+c+10)){
+        return 5;
+    } 
 }
