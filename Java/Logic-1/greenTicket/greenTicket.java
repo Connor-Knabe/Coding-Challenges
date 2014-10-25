7eb7aa91da407b87b25aec903b22ca006a409066
Connor Knabe <ConnorKnabe@gmail.com>
2014-10-25 11:04:43 -0500

    All tests pass

--- Java/Logic-1/greenTicket/greenTicket.java
+++ Java/Logic-1/greenTicket/greenTicket.java
@@ -6,10 +6,11 @@ greenTicket(1, 1, 2) → 10
 */
 
 public int greenTicket(int a, int b, int c) {
-    if (a!=b && b!=c){
+    if (a!=b && b!=c && c!=a){
         return 0;
     } else if (a==b&&b==c){
-    
         return 20;
+    } else {
+        return 10;
     }
 }
