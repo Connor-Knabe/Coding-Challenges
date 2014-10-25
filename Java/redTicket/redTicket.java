48aac8a80a11ea136f68119cc2a88fdd7e704027
Connor Knabe <ConnorKnabe@gmail.com>
2014-10-25 10:53:30 -0500

    Take care of rest of cases

--- Java/redTicket/redTicket.java
+++ Java/redTicket/redTicket.java
@@ -9,5 +9,9 @@ redTicket(0, 0, 0) → 5
 public int redTicket(int a, int b, int c) {
     if (a==2 && b==2 && c==2){
         return 10;
-    }
+    } else if (a==b && b==c){
+        return 5;
+    } else {
+        return 0;
+    }    
 }
