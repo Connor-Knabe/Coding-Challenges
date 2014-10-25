f14de2206a4b86906be1131d2ddcf18c4fa11dc9
Connor Knabe <ConnorKnabe@gmail.com>
2014-10-25 10:42:45 -0500

    All tests pass

--- Java/Logic-1/maxMod5.java
+++ Java/Logic-1/maxMod5.java
@@ -7,12 +7,15 @@ maxMod5(3, 2) → 3
 */
 public int maxMod5(int a, int b) {
 
-    if (a%5 == b%5){
-        
-    }  
+ 
     if (a==b){
         return 0;
     }
+    if (a%5 == b%5){
+        if(a<b) return a;
+        else return b;
+        
+    } 
     if (a < b){
         return b;
     } else {
