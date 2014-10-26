c03cbc029a193a3e70e69e90634a927a9af1f70b
Connor Knabe <ConnorKnabe@gmail.com>
2014-10-26 11:50:46 -0500

    All tests pass

--- Java/Logic-1/withoutDoubles/withoutDoubles.java
+++ Java/Logic-1/withoutDoubles/withoutDoubles.java
@@ -8,8 +8,11 @@ withoutDoubles(3, 3, false) → 6
 public int withoutDoubles(int die1, int die2, boolean noDoubles) {
 
     if (noDoubles && (die1 == die2)){
+        if (die1 == 6 || die2 == 6){
+            return (die1+1);
+        }
         return (die1+die2+1);
     } else {
-        return die1+die2;
+        return (die1+die2);
     }
 }
