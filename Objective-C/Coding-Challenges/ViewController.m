227c063417784150c39266113f3cad14758ffb84
Connor Knabe <ConnorKnabe@gmail.com>
2014-10-27 10:08:38 -0500

    Added test inputs and instructions

--- Objective-C/Coding-Challenges/ViewController.m
+++ Objective-C/Coding-Challenges/ViewController.m
@@ -16,9 +16,21 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    
+    /*
+     Given a string str, if the string starts with "f" return "Fizz". If the string ends with "b" return "Buzz". If both the "f" and "b" conditions are true, return "FizzBuzz". In all other cases, return the string unchanged.
+     
+     Test inputs
+     fizzString("fig") → "Fizz"
+     fizzString("dib") → "Buzz"
+     fizzString("fib") → "FizzBuzz"
+     */
     
     NSString* str = @"fizz";
+    
+    
+    
+    
+    
     
     NSLog(@"TEST");
     
