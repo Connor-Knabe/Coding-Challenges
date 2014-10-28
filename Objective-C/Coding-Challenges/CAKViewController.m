76cd59219450d3a4c2ccb4dff0d4f263eeabbc85
Connor Knabe <ConnorKnabe@gmail.com>
2014-10-28 09:31:40 -0500

    Made it pass

--- Objective-C/Coding-Challenges/CAKViewController.m
+++ Objective-C/Coding-Challenges/CAKViewController.m
@@ -39,7 +39,9 @@
     fizzString2(3) → "Fizz!"
     */
     
-    if (n % 3 == 0){
+    if (n % 3 ==0 && n % 5 ==0 ){
+        return @"FizzBuzz!";
+    } else if (n % 3 == 0){
         return @"Fizz!";
     } else if (n % 5 == 0) {
         return @"Buzz!";
