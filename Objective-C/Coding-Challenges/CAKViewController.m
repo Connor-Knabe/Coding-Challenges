55debd21ec7a3f7bba0c528d5d0be0272662fa6f
Connor Knabe <ConnorKnabe@gmail.com>
2014-10-29 09:32:21 -0500

    made the test pass

--- Objective-C/Coding-Challenges/CAKViewController.m
+++ Objective-C/Coding-Challenges/CAKViewController.m
@@ -90,12 +90,18 @@
     NSInteger sum = 0;
     if (a != 13){
         sum += a;
+    } else {
+        return sum;
     }
     if (b != 13){
         sum += b;
+    } else {
+        return sum;
     }
     if (c != 13){
         sum += c;
+    } else {
+        return sum;
     }
     return sum;
 }
