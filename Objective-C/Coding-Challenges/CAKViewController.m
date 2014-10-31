e8b7eb306e1551969bff4036317028abe979086e
Connor Knabe <ConnorKnabe@gmail.com>
2014-10-31 07:27:28 -0400

    Started on firstLast6 problem

--- Objective-C/Coding-Challenges/CAKViewController.m
+++ Objective-C/Coding-Challenges/CAKViewController.m
@@ -127,6 +127,25 @@
 
 
 
+//Codingbat solutions Array-1 Start ----
+
+/* 
+ Given an array of ints, return true if 6 appears as either the first or last element in the array. The array will be length 1 or more.
+ 
+ firstLast6({1, 2, 6}) → true
+ firstLast6({6, 1, 2, 3}) → true
+ firstLast6({13, 6, 1, 2, 3}) → false
+
+ */
+- (BOOL)firstLast6:(NSArray*)nums {
+    
+    
+    return NO;
+}
+
+
+//Codingbat solutions Array-1 End ----
+
 
 
 - (void)didReceiveMemoryWarning {
