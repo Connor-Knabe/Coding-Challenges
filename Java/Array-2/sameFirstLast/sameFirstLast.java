b4d5f5ef096a3e377abf0c7a32dbffd72dbe69f4
Connor Knabe <ConnorKnabe@gmail.com>
2014-11-01 08:36:12 -0400

    Codingbats simple 1 line solution

--- Java/Array-2/sameFirstLast/sameFirstLast.java
+++ Java/Array-2/sameFirstLast/sameFirstLast.java
@@ -9,16 +9,9 @@ sameFirstLast({1, 2, 1}) → true
 */
 
 public boolean sameFirstLast(int[] nums) {
-
-
-  if(nums.length < 1) {
-  
-      return false;
-  }  
-  if(nums[0] == nums[nums.length-1]){
-      return true;
-  }else{
-      return false;
-  }
+  return (nums.length >= 1 && nums[0] == nums[nums.length - 1]);
+   
+  // Solution notes: this is basically a direct translation of the problem
+  // statement. The length check needs to come first, so we don't get an
+  // error in the case that the length is 0.
 }
-
