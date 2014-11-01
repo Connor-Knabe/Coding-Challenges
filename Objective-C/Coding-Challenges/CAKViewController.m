2f210589f16512f6aaa3238d070cbaa0e3a9466f
Connor Knabe <ConnorKnabe@gmail.com>
2014-11-01 08:26:24 -0400

    Started on sameFirstLast

--- Objective-C/Coding-Challenges/CAKViewController.m
+++ Objective-C/Coding-Challenges/CAKViewController.m
@@ -147,6 +147,20 @@
     }    
 }
 
+/*
+ 
+ Given an array of ints, return true if the array is length 1 or more, and the first element and the last element are equal.
+ 
+ sameFirstLast({1, 2, 3}) → false
+ sameFirstLast({1, 2, 3, 1}) → true
+ sameFirstLast({1, 2, 1}) → true
+ */
+
+- (BOOL)sameFirstLast:(NSArray*)nums{
+    
+    return NO;
+}
+
 
 //Codingbat solutions Array-1 End ----
 
