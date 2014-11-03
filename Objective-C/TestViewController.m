224e888d6fc59314f31c209da4249b3297a4dfba
Connor Knabe <ConnorKnabe@gmail.com>
2014-11-03 09:48:11 -0600

    Wrote another passing test

--- Objective-C/TestViewController.m
+++ Objective-C/TestViewController.m
@@ -201,7 +201,6 @@
 - (void)testCommonTwoReturns2GivenArrayWithTwoSameCharacters {
     NSArray* arrA = [[NSArray alloc]initWithObjects:@"a",@"c",@"x", nil];
     NSArray* arrB = [[NSArray alloc]initWithObjects:@"b",@"c",@"d",@"x", nil];
-
     NSInteger result = [self.testObj commonTwo:arrA withBArray:arrB];
     XCTAssert(result == 2);
 }
@@ -210,10 +209,20 @@
 - (void)testCommonTwoReturns3GivenArrayWithThreeSameCharacters {
     NSArray* arrA = [[NSArray alloc]initWithObjects:@"a",@"b",@"c", nil];
     NSArray* arrB = [[NSArray alloc]initWithObjects:@"a",@"b",@"c", nil];
-    
     NSInteger result = [self.testObj commonTwo:arrA withBArray:arrB];
     XCTAssert(result == 3);
 }
+
+- (void)testCommonTwoReturns3GivenArrayWithThreeSameCharactersPlusExtra {
+    NSArray* arrA = [[NSArray alloc]initWithObjects:@"a",@"b",@"c", nil];
+    NSArray* arrB = [[NSArray alloc]initWithObjects:@"a",@"b",@"c",@"x",@"z", nil];
+    NSInteger result = [self.testObj commonTwo:arrA withBArray:arrB];
+    XCTAssert(result == 3);
+}
+
+
+
+
 //Codingbat tests AP-1 End ----
 
 @end
