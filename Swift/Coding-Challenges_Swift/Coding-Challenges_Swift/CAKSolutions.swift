ffbc62f69f26de1b159f9fe6491accbd9ce6c8d9
Connor Knabe <ConnorKnabe@gmail.com>
2014-11-05 09:11:27 -0600

    Created a playground to experiment with strings

--- Swift/Coding-Challenges_Swift/Coding-Challenges_Swift/CAKSolutions.swift
+++ Swift/Coding-Challenges_Swift/Coding-Challenges_Swift/CAKSolutions.swift
@@ -6,6 +6,15 @@ import Foundation
 class CAKSolutions {
     
     func fizzBuzz(str: String)->String {
+        
+        
+        let ss1: String = str.substringToIndex(1);
+        
+        
+        if (str.substringToIndex(1) == "f"){
+            
+            
+        }
         return str;
     }
     
---
+++ Swift/Coding-Challenges_Swift/MyPlayground.playground/section-1.swift
@@ -0,0 +1,9 @@
+// Playground - noun: a place where people can play
+
+import UIKit
+
+var str = "Hello, playground"
+
+let str: String = "funny"
+let ss1: String = str.substringToIndex(1)
+
