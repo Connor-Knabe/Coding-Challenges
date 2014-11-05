78b14b02d1a2591574199bb6fa38d3738ec6d74e
Connor Knabe <ConnorKnabe@gmail.com>
2014-11-05 09:42:59 -0600

    Both tests pass

--- Objective-C/Coding-Challenges/CAKViewController.m
+++ Objective-C/Coding-Challenges/CAKViewController.m
@@ -208,7 +208,18 @@
 
 - (NSInteger)commonTwo:(NSArray*)a withBArray:(NSArray*)b{
     /* Start with two arrays of strings, a and b, each in alphabetical order, possibly with duplicates. Return the count of the number of strings which appear in both arrays. The best "linear" solution makes a single pass over both arrays, taking advantage of the fact that they are in alphabetical order. */
-    return 2;
+    NSInteger sum = 0;
+    
+    for (NSInteger i=0; i<[a count]; i++){
+        for (NSInteger j=0; j<[b count]; j++){
+            if ([a[i] isEqualToString:b[j]]){
+                sum++;
+            }
+        }
+        
+    }
+    
+    return sum;
 
 }
 
