808a56ccf587aa8d85f71385618f8665e55ced64
Connor Knabe <ConnorKnabe@gmail.com>
2014-11-05 08:56:04 -0600

    Wrote a failing test

--- Swift/Coding-Challenges_Swift/Coding-Challenges_Swift/TestViewController.swift
+++ Swift/Coding-Challenges_Swift/Coding-Challenges_Swift/TestViewController.swift
@@ -10,16 +10,19 @@ import UIKit
 import XCTest
 
 class TestViewController: XCTestCase {
+    var testObj :CAKSolutions?
 
     override func setUp() {
         super.setUp()
-        let testObj = CAKSolutions()
+        testObj = CAKSolutions()
+        
         // Put setup code here. This method is called before the invocation of each test method in the class.
     }
     
   
     func testFizzBuzz(){
-        
+        var result = testObj?.fizzBuzz("TEST");
+        XCTAssert(result == "TEST");
         
     }
     
