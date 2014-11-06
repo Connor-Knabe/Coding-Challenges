393f0396252b5060c6db6441be837db2308dcc90
Connor Knabe <ConnorKnabe@gmail.com>
2014-11-06 11:56:00 -0600

    started on makepi

---
+++ Java/Array-1/makePi/makePi.java
@@ -0,0 +1,10 @@
+/*Return an int array length 3 containing the first 3 digits of pi, {3, 1, 4}. 
+
+makePi() → {3, 1, 4}*/
+
+public class makePi {
+    public static void main(String[] args) {
+        
+        
+    }
+}
