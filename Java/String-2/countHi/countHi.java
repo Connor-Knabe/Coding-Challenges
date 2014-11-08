9eecfa9e4cd8bca0af82430fa46b6ffac1dba272
Connor Knabe <ConnorKnabe@gmail.com>
2014-11-08 09:03:36 -0600

    Codingbat version

--- Java/String-2/countHi/countHi.java
+++ Java/String-2/countHi/countHi.java
@@ -5,18 +5,14 @@ countHi("ABChi hi") → 2
 countHi("hihi") → 2
 */
 
-public class countHi {
-    public static void main(String[] args) {
-        String str = "hihi";
-        int sum = 0;
-        String strhi = "";
-        for(int i=0;i<str.length()-1;i++){
-            strhi = str.substring(i,i+2);
-            if (strhi.equals("hi")){
-                sum++;
-            }
-            System.out.println(strhi);
+public int countHi(String str) {
+    int sum = 0;
+    String strhi = "";
+    for(int i=0;i<str.length()-1;i++){
+        strhi = str.substring(i,i+2);
+        if (strhi.equals("hi")){
+            sum++;
         }
-        System.out.println(sum);
     }
+    return (sum);
 }
