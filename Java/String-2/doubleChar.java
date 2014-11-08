2e0d2b76da8c38714f3688e419133075d614eb48
Connor Knabe <ConnorKnabe@gmail.com>
2014-11-08 08:45:50 -0600

    Copies the word but only once

--- Java/String-2/doubleChar.java
+++ Java/String-2/doubleChar.java
@@ -10,7 +10,7 @@ public class doubleChar {
     	String str = "The";
     	String str2 = "";
     	for (int i=0; i<str.length();i++){
-    		str2 += str.charAt(i) + str.charAt(i);
+    		str2 += str.charAt(i);
     	}    
     	System.out.println(str2);
         
