433bf3d339f47bb847a5284bc8bbef0f321e0e1d
Connor Knabe <ConnorKnabe@gmail.com>
2014-11-08 08:43:43 -0600

    Loop through the string and add char to new string

--- Java/String-2/doubleChar.java
+++ Java/String-2/doubleChar.java
@@ -1,6 +1,17 @@
+/*Given a string, return a string where for every char in the original, there are two chars. 
+
+doubleChar("The") → "TThhee"
+doubleChar("AAbb") → "AAAAbbbb"
+doubleChar("Hi-There") → "HHii--TThheerree"
+*/
+
 public class doubleChar {
     public static void main(String[] args) {
-        
+    	String str = "The";
+    	String str2 = "";
+    	for (int i=0; i<str.length();i++){
+    		str2 += str.charAt(i) + str.charAt(i);
+    	}    
         
     }
 }
