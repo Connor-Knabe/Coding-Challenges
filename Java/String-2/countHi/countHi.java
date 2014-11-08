dac48bc918bab6e1bcdc4d74cbfa4bdd78f58342
Connor Knabe <ConnorKnabe@gmail.com>
2014-11-08 08:52:23 -0600

    Added instructions

--- Java/String-2/countHi/countHi.java
+++ Java/String-2/countHi/countHi.java
@@ -1,6 +1,13 @@
+/*Return the number of times that the string "hi" appears anywhere in the given string.
+
+countHi("abc hi ho") → 1
+countHi("ABChi hi") → 2
+countHi("hihi") → 2
+*/
+
 public class countHi {
     public static void main(String[] args) {
-        
-        
+
+
     }
 }
