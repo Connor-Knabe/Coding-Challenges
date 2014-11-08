dfe11bd7ed6870fcfafacd1ab836fbe04736a5b8
Connor Knabe <ConnorKnabe@gmail.com>
2014-11-08 08:57:16 -0600

    Printing out substring

--- Java/String-2/countHi/countHi.java
+++ Java/String-2/countHi/countHi.java
@@ -9,7 +9,7 @@ public class countHi {
     public static void main(String[] args) {
         String str = "hihi";
         int sum = 0;
-        for(int i=0;i<str.length()-1;i++){
+        for(int i=0;i<str.length();i++){
             System.out.println(str.substring(i,i+1));
         }
         System.out.println(sum);
