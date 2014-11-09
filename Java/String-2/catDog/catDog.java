3da1bfb2c20a93a6a2b9d9607b74ac703f45c9c0
Connor Knabe <ConnorKnabe@gmail.com>
2014-11-09 10:28:32 -0600

    Adding up number of times dog appears

--- Java/String-2/catDog/catDog.java
+++ Java/String-2/catDog/catDog.java
@@ -8,10 +8,16 @@ catDog("1cat1cadodog") → true*/
 public class catDog {
     public static void main(String[] args) {
     int catSum = 0;
+    int dogSum = 0;
+
     for(int i=0;i<str.length()-2;i++){
         if (str.substring(i,i+3).equals("cat")){
             catSum++;
         }
+        if (str.substring(i,i+3).equals("dog")){
+            dogSum++;
+        }
+
     }
     return (sum);
 
