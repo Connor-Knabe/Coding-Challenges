7c4bad58c0da5a9bc3fb0daa70793004e51c7295
Connor Knabe <ConnorKnabe@gmail.com>
2014-11-09 10:29:15 -0600

    All tests pass

--- Java/String-2/catDog/catDog.java
+++ Java/String-2/catDog/catDog.java
@@ -4,10 +4,8 @@ Return true if the string "cat" and "dog" appear the same number of times in the
 catDog("catdog") → true
 catDog("catcat") → false
 catDog("1cat1cadodog") → true*/
-
-public class catDog {
-    public static void main(String[] args) {
-    int catSum = 0;
+public boolean catDog(String str) {
+  int catSum = 0;
     int dogSum = 0;
 
     for(int i=0;i<str.length()-2;i++){
@@ -17,9 +15,8 @@ public class catDog {
         if (str.substring(i,i+3).equals("dog")){
             dogSum++;
         }
-
     }
-    return (sum);
+    return (catSum == dogSum);
 
-    }
+
 }
