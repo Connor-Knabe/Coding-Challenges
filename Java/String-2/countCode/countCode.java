24ba4c8761674ed4173eaa14dff81789efc436ea
Connor Knabe <ConnorKnabe@gmail.com>
2014-11-09 19:14:21 -0600

    Checking each character to see if it lines up with c o * e

--- Java/String-2/countCode/countCode.java
+++ Java/String-2/countCode/countCode.java
@@ -1,6 +1,19 @@
+/*Return the number of times that the string "code" appears anywhere in the given string, except we'll accept any letter for the 'd', so "cope" and "cooe" count.
+
+countCode("aaacodebbb") → 1
+countCode("codexxcode") → 2
+countCode("cozexxcope") → 2*/
+
 public class countCode {
     public static void main(String[] args) {
+        String str = "aaacodebbb";
+        int sum = 0;
+        for (int i = 0; i<str.length();i++){
+            if(str.charAt(i)=="c" && str.charAt(i+1)=="o" && str.charAt(i+3)){
+                sum++;
+            }
 
+        }
 
     }
 }
