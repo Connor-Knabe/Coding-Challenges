a91ec11eb38cb54968821e1d9f5c30d399405059
Connor Knabe <ConnorKnabe@gmail.com>
2014-11-09 19:18:12 -0600

    Codingbat format some tests fail because out of bounds

--- Java/String-2/countCode/countCode.java
+++ Java/String-2/countCode/countCode.java
@@ -4,16 +4,13 @@ countCode("aaacodebbb") → 1
 countCode("codexxcode") → 2
 countCode("cozexxcope") → 2*/
 
-public class countCode {
-    public static void main(String[] args) {
-        String str = "cozexxcope";
-        int sum = 0;
-        for (int i = 0; i<str.length();i++){
-            if(str.charAt(i)=='c' && str.charAt(i+1)=='o' && str.charAt(i+3)=='e'){
-                sum++;
-            }
-
+public int countCode(String str) {
+    int sum = 0;
+    for (int i = 0; i<str.length();i++){
+        if(str.charAt(i)=='c' && str.charAt(i+1)=='o' && str.charAt(i+3)=='e'){
+            sum++;
         }
-        System.out.println(sum);
     }
+    return sum;
+
 }
