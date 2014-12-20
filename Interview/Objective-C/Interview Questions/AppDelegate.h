1699f271390cfaad93875914784da7e1267920bd
Connor Knabe <ConnorKnabe@gmail.com>
2014-12-19 18:49:29 -0600

    Working on Objective-C interview questions

---
+++ Interview/Objective-C/Interview Questions/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Interview Questions
+//
+//  Created by Connor Knabe on 12/19/14.
+//  Copyright (c) 2014 Connor Knabe. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Interview/Objective-C/Interview Questions/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Interview Questions
+//
+//  Created by Connor Knabe on 12/19/14.
+//  Copyright (c) 2014 Connor Knabe. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
