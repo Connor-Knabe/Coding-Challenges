020e2e9230e77b01b3811a3a72e47e4ab7107db5
Connor Knabe <ConnorKnabe@gmail.com>
2014-12-19 19:03:14 -0600

    Wrote a failing test

---
+++ Interview/Objective-C/Interview Questions/TestClass.m
@@ -0,0 +1,54 @@
+//
+//  TestClass.m
+//  Interview Questions
+//
+//  Created by Connor Knabe on 12/19/14.
+//  Copyright (c) 2014 Connor Knabe. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+#import "ViewController.h"
+
+@interface TestClass : XCTestCase
+
+@property (nonatomic)ViewController* testObj;
+
+@end
+
+@implementation TestClass
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+    
+    self.testObj = [[ViewController alloc]init];
+    
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    XCTAssert(YES, @"Pass");
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+- (void)testFizzBuzzReturns_F_Given_Fizz {
+    
+    NSString* result = [self.testObj fizzBuzz:@"fizz"];
+    
+    XCTAssertTrue([result isEqualToString:(@"F")]);
+    
+    
+}
+
+@end
--- Interview/Objective-C/Interview Questions/ViewController.m
+++ Interview/Objective-C/Interview Questions/ViewController.m
@@ -8,7 +8,9 @@
 
 #import "ViewController.h"
 
+
 @interface ViewController ()
+
 
 @end
 
@@ -17,11 +19,15 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
+
+
 }
 
-- (void)didReceiveMemoryWarning {
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
+- (NSString*)fizzBuzz:(NSString*)word {
+    
+    return @"t";
 }
+
+
 
 @end
