bb63fa3c404276496e823c166689e3481fae2cc8
Connor Knabe <ConnorKnabe@gmail.com>
2014-12-19 19:14:20 -0600

    Made test pass

--- Interview/Objective-C/Interview Questions/ViewController.m
+++ Interview/Objective-C/Interview Questions/ViewController.m
@@ -28,6 +28,8 @@
     BOOL f = [[word substringToIndex:1] isEqualToString:@"f"];
     BOOL b = [[word substringFromIndex:word.length-1] isEqualToString:@"b"];
 
+    if (f&&b)
+        return @"FizzBuzz";
     if (b)
         return @"b";
     if (f)
