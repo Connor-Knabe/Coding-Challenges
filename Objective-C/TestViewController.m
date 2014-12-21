5a2f7e05267683b017744b4c74f967caed4da10d
Connor Knabe <ConnorKnabe@gmail.com>
2014-12-21 14:31:08 -0600

    Removed a test

--- Objective-C/TestViewController.m
+++ Objective-C/TestViewController.m
@@ -249,12 +249,6 @@
 }
 
 
-- (void)testCommonTwoReturns3GivenArrayWith3SameCharactersAndManyDuplicates {
-    NSArray* arrA = [[NSArray alloc]initWithObjects:@"a",@"a",@"b",@"b",@"c", nil];
-    NSArray* arrB = [[NSArray alloc]initWithObjects:@"a",@"b",@"b",@"b",@"c", nil];
-    NSInteger result = [self.testObj commonTwo:arrA withBArray:arrB];
-    XCTAssert(result == 3);
-}
 
 
 
