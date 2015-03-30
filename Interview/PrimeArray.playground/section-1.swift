13aaeadfe5f16934eeb321c4ec55d6389e0111ea
Connor Knabe <ConnorKnabe@gmail.com>
2015-03-30 16:35:16 -0500

    Added a new challenege to add first 100 prime numbers to array

---
+++ Interview/PrimeArray.playground/section-1.swift
@@ -0,0 +1,31 @@
+// Playground - noun: a place where people can play
+
+import UIKit
+
+var str = "Hello, playground"
+
+
+var array = [Int]()
+var maxIndex = 0;
+
+
+
+func isPrime(num: Int)->Bool{
+    for(var i = 2; i<num;i++){
+        
+        if (num%i==0){
+            return false;
+        }
+    }
+    return true;
+}
+
+
+for(var i = 1; maxIndex < 100; i += 2){
+    if(isPrime(i)){
+        array.append(i)
+        maxIndex++
+        println(i)
+    }
+
+}
